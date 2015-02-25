f99f201010d79266da8ced307bf693daeb30d6c3
MrLeeh <Stefan.St.Lehmann@gmail.com>
2015-02-25 10:56:28 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #! python3
 from setuptools import setup
+from pdftools import __version__
 
 setup(
     name='pdftools',
-    version='1.0.2',
+    version=__version__,
     packages=['pdftools'],
-    scripts=['pdfsplit.py', 'pdfmerge.py', 'pdfrotate.py', 'pdfzip.py'],
+    scripts=['pdfsplit.py', 'pdfmerge.py', 'pdfrotate.py', 'pdfzip.py',
+             'pdfinsert.py'],
     url='https://github.com/MrLeeh/pdftools',
     license='MIT',
     author='Stefan Lehmann',
     author_email='Stefan.St.Lehmann@gmail.com',
-    description='small collection of convenience scripts for pdf manipulation',
+    description='A collection of convenience scripts for pdf manipulation',
     install_requires=['PyPdf2'],
     maintainer='Stefan Lehmann'
 )
