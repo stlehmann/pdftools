8de14595e7077a422d94f2b9c9549d13d8064ed5
MrLeeh <Stefan.St.Lehmann@gmail.com>
2015-09-04 10:13:54 +0200

    autoconvert README.md to README.rst on upload or register

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
+import os
+import sys
 from setuptools import setup
 import versioneer
+
+
+# use pandoc to convert README.md to README.rst
+if any(s in sys.argv for s in ("upload", "register")):
+    os.system("pandoc {} -o {}".format("README.md", "README.rst"))
+
+
+if os.path.exists("README.rst"):
+    long_description = open("README.rst").read()
+    os.remove("README.rst")
+else:
+    long_description = ""
+
 
 setup(
     name='pdftools',
@@ -14,6 +29,7 @@ setup(
     author_email='Stefan.St.Lehmann@gmail.com',
     description='A collection of convenience scripts for pdf manipulation, '
                 'based on the PyPdf2 package.',
+    long_description=long_description,
     install_requires=['PyPdf2'],
     maintainer='Stefan Lehmann',
     cmdclass=versioneer.get_cmdclass()
